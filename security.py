98999612165a1491af4d7f47843d27ca60c1c32d
Lars Eschweiler <vreezy117@googlemail.com>
2024-07-14 20:00:12 +0200

    prev cam

--- security.py
+++ security.py
@@ -1,6 +1,7 @@
 from gpiozero import LED,Button
+from picamera2 import Picamera2, Preview
 from time import sleep
-from picamera2 import Picamera2
+
 
 picam2 = Picamera2()
 
@@ -11,12 +12,17 @@ motionSensor = Button(21)
 while True:
     if button.is_pressed:
         red.on()
+        picam2.start_preview(Preview.QTGL)
+        picam2.start()
+        sleep(5)
+        picam2.close()
+        # picam2.start_and_capture_file("Desktop/new_image.jpg")
+        # picam2.close()
     else:
         red.off()
     if motionSensor.is_pressed:
         red.on()
-        picam2.start_and_capture_file("Desktop/new_image.jpg")
-        picam2.close()
+
     else:
         red.off()
 
