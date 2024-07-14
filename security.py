7beb0dfca56f18500166fa9b724b4767db049efc
Lars Eschweiler <vreezy117@googlemail.com>
2024-07-14 19:11:29 +0200

    init

---
+++ security.py
@@ -0,0 +1,28 @@
+# from picamera2 import Picamera2, Preview
+from time import sleep
+from gpiozero import LED, Button
+# import RPi.GPIO as GPIO
+
+button = Button(0)
+button.when_presse = 
+
+if button.is_pressed:
+  print("Player 1 wins!")
+break
+
+# SHUTDOWN = 0
+# LED = 24
+# RESET = 16
+# POWER = 20
+# STATUSQ = 21
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+# GPIO.setup(LED, GPIO.OUT, initial=GPIO.HIGH)
+
+
+# picam2 = Picamera2()
+# picam2.start_preview(Preview.QTGL)
+# picam2.start()
+# sleep(5)
+# picam2.close()
