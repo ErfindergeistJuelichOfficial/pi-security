54e1f4e10454a529d97e077f32d8b43600cf0b23
Lars Eschweiler <vreezy117@googlemail.com>
2024-07-14 19:36:01 +0200

    tut code and wirering

--- security.py
+++ security.py
@@ -1,12 +1,16 @@
-from gpiozero import LED
+from gpiozero import LED,Button
 from time import sleep
 
 red = LED(17)
+button = Button(2)
 
 while True:
-    red.on()
-    sleep(1)
-    red.off()
+    if button.is_pressed:
+        print("Button is pressed")
+        red.on()
+    else:
+        print("Button is not pressed")
+        red.off()
 
 
 # https://gpiozero.readthedocs.io/en/latest/
