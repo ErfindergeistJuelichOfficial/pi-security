47e1e721c723056d79b631ce99eeb7dd8cca71e9
Lars Eschweiler <vreezy117@googlemail.com>
2024-07-14 20:03:37 +0200

    cam

--- security.py
+++ security.py
@@ -1,6 +1,8 @@
 from gpiozero import LED,Button
 from picamera2 import Picamera2, Preview
-from time import sleep
+from time import sleep, strftime
+
+
 
 
 picam2 = Picamera2()
@@ -12,9 +14,8 @@ motionSensor = Button(21)
 while True:
     if button.is_pressed:
         red.on()
-        picam2.start_preview(Preview.QTGL)
-        picam2.start()
-        sleep(5)
+        timestr = strftime("%Y%m%d-%H%M%S")
+        picam2.start_and_capture_files("Desktop/sequence_timestr_{:d}.jpg", num_files=3, delay=0.5)
         picam2.close()
         # picam2.start_and_capture_file("Desktop/new_image.jpg")
         # picam2.close()
