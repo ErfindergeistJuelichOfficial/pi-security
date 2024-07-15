438c6d471f7eecdc77ac800243a20189dfc7cf3d
Lars Eschweiler <l.eschweiler@novacapta.de>
2024-07-15 08:51:25 +0200

    todos

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# pi-security
-Erfindergeist PI-Security
+# Erfindergeist PI-Security
+
+## TODO
+
+- add electric wire plan
+- 
+
+## setup
+
+- git clone this project
+- copy example.env to .env ```cp example.env .env```
+- setup a target FTP
+- edit .env and set the settings ```nano .env```
+
--- security.py
+++ security.py
@@ -5,26 +5,42 @@ from time import sleep, strftime
 from dotenv import load_dotenv
 import ftplib
 
+## setup env
 load_dotenv()
 
 FTP_USR = os.getenv('FTP_USR')
 FTP_PWD = os.getenv('FTP_PWD')
 FTP_URL = os.getenv('FTP_URL')
 
-print("ftp host:")
-print(FTP_URL)
-
-picam2 = Picamera2()
-
+## setup GPIO
 red = LED(17)
 button = Button(2)
 motionSensor = Button(21)
 
+## setup camera
+picam2 = Picamera2()
+
+## program loop
+# TODO write functions
+# TODO motion is alway on. off is a detection
+# todo send pics only every 5 minutes (day mode)
+# TODO time detector between 8pm an 8am send more pics (night mode)
+# TODO add API to save events
+# todo delete pics on usb after x days. maybe an additional script than runs daily and check?
+# todo delete pics on ftp after x days. maybe an additional script than runs daily and check?
+# TODO use current button to make a manuel pic
+# TODO add shutdown button
+# TODO red LED to show system is ready
+# TODO blue LED to show motion detected
+# TODO green LED to show pic on upload
+# TODO yellow LED to show night/day mode)
+
 while True:
     if button.is_pressed:
         red.on()
-        timestr = strftime("%Y%m%d-%H%M%S")
-        filename = "motion_" + timestr + ".jpg"
+        timeStr = strftime("%Y%m%d-%H%M%S")
+        filename = "motion_" + timeStr + ".jpg"
+        # TODO save pic on USB
         picam2.start_and_capture_files(filename)
 
         try:
