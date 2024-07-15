96ebc5e2a9906f1064c3d0263af12a7f905b1241
Lars Eschweiler <vreezy117@googlemail.com>
2024-07-15 20:52:04 +0200

    shutdown

--- security.py
+++ security.py
@@ -3,6 +3,7 @@ from gpiozero import LED,Button
 from picamera2 import Picamera2, Preview
 from time import sleep, strftime
 from dotenv import load_dotenv
+from subprocess import check_call
 import ftplib
 
 ## setup env
@@ -18,26 +19,30 @@ blue = LED(16)
 green = LED(20)
 yellow = LED(21)
 button = Button(2)
+shutdown_btn = Button(3, hold_time=2)
 motionSensor = Button(26)
 
 ## setup camera
 picam2 = Picamera2()
 
 ## program loop
-# TODO write functions
-# TODO motion is alway on. off is a detection
+
+
+
+
 # TODO save pic on USB
 # TODO send pics only every 5 minutes (day mode)
 # TODO time detector between 8pm an 8am send more pics (night mode)
+
 # TODO add API to save events
+
 # TODO delete pics on usb after x days. maybe an additional script than runs daily and check?
 # TODO delete pics on ftp after x days. maybe an additional script than runs daily and check?
-# TODO use current button to make a manuel pic
 # TODO add shutdown button
-# TODO red LED to show system is ready
-# TODO blue LED to show motion detected
-# TODO green LED to show pic on upload
 # TODO yellow LED to show night/day mode)
+
+def shutdown():
+    check_call(['sudo', 'poweroff'])
 
 def capture():
     time_str = strftime("%Y%m%d-%H%M%S")
@@ -60,8 +65,9 @@ def capture():
     
 while True:
     red.on()
+    shutdown_btn.when_held = shutdown
     if button.is_pressed:
-        capture()
+        capture
     if motionSensor.is_pressed:
         blue.off()
 
